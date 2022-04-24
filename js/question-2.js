eeffd0b0150bbab75515e25423317df27b083bb4
piyu010 <piyush.sharma@stud.noroff.no>
2022-04-24 23:49:21 +0200

    Create question-2.js

---
+++ js/question-2.js
@@ -0,0 +1,37 @@
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=69b1ed4176de461ca39d2b73f3da3439";
+const proxy = "https://noroffcors.herokuapp.com/";
+
+const corsFix = proxy + url;
+
+const resultsContainter = document.querySelector(".results");
+
+async function getRawg() {
+    try {
+
+        const response = await fetch(url);
+
+        const results = await response.json();
+
+        const games = results.results;
+
+        resultsContainter.innerHTML = ""
+
+        for (let i = 0; i < games.length; i++) {
+            console.log(games[i].name);
+            console.log(games[i].rating);
+            console.log(games[i].tags.length);
+            if (i === 8) {
+                break;
+            }
+
+            resultsContainter.innerHTML += `<div class="result">${games[i].name}<br>${games[i].rating}<br>${games[i].tags.length}</div>`;
+
+        }
+    } catch (error) {
+        console.log(error);
+        resultsContainer.innerHTML = alert("error", error);
+    }
+}
+
+getRawg();
